69d864951defdbbbbc4d4ba67bb022894a2242ba
Kaikaine <kairijb@protonmail.com>
2020-04-23 15:59:44 -0500

    create player input function

--- game.py
+++ game.py
@@ -10,4 +10,20 @@ test_board = ['a', 'b', 'c',
               'a', 'b', 'c',
               'a', 'b', 'c', ]
 
+
+def player_input():
+    marker = ''
+
+    while marker != 'X' and marker != 'O':
+        marker = input('Player1: Choose X or O: ').upper()
+
+    if marker == 'X':
+        return ('X', 'O')
+    else:
+        return ('O', 'X')
+
+
+player1_marker, player2_marker = player_input()
+
 display(test_board)
+player_input()
