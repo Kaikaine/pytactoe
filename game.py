e3db2e138de79bd19bb586ad97e2b9b13c6e377e
Kaikaine <kairijb@protonmail.com>
2020-04-23 17:46:37 -0500

    create function that checks for a winner

--- game.py
+++ game.py
@@ -25,5 +25,25 @@ def player_input():
 
 player1_marker, player2_marker = player_input()
 
-display(test_board)
+
+def place_marker(board, marker, position):
+    board[position] = marker
+
+
+def win_check(board, mark):
+    return ((board[0] == board[1] == board[2] == mark) or
+            (board[3] == board[4] == board[5] == mark) or
+            (board[6] == board[7] == board[8] == mark) or
+
+            (board[0] == board[3] == board[6] == mark) or
+            (board[1] == board[4] == board[7] == mark) or
+            (board[2] == board[5] == board[8] == mark) or
+
+            (board[6] == board[4] == board[2] == mark) or
+            (board[8] == board[4] == board[0] == mark))
+
+
 player_input()
+place_marker(test_board, '$', 8)
+display(test_board)
+win_check(test_board, 'X')
